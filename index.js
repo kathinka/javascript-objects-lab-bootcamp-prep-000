f46a668c38c53e4649c94f569598aad133e9aab3
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-03 19:52:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function updateObjectWithKeyAndValue(object, key, value){
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
-  recipes[key] = 2;
+  recipes.prop2 = 2;
   return recipes;
 }
 
