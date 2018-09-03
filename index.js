1ed5120ddd73ef3ba4628bdea705b0057e4e62af
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-03 19:55:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 
 function deleteFromObjectByKey (object, key){
   var newRecipes = Object.assign({},recipes);
-  delete newRecipes.key;
+  delete newRecipes[key];
   return newRecipes;
 }
 /*
