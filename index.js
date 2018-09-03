8b6c9bbb4df53662be596770dbfb964360f567aa
kathinka martinsen <kathinkamartinsen@gmail.com>
2018-09-03 19:57:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function deleteFromObjectByKey (object, key){
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
-  delete recipes.key;
+  delete recipes.prop;
   return recipes;
 }
 /*
